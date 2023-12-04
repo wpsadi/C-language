d510859e808550c24eada85fe1a5a7f92de73b6b
Aditya <118204758+wpsadi@users.noreply.github.com>
2023-12-04 10:58:43 +0000

    save

---
+++ Practical/Q012/sol.c
@@ -0,0 +1,13 @@
+// pattern using C
+
+#include <stdio.h>
+
+int main(){
+    for (int i = 0; i <4; i++){
+        for (int j = 0; j<=i ; j++){
+            printf("*");
+        }
+        printf("\n");
+    }
+    return 0;
+}

b3957dec876bc5e80aba828f4ff6fd2a025da5a8
Aditya <118204758+wpsadi@users.noreply.github.com>
2023-12-04 10:52:15 +0000

    save

---
+++ Practical/Q010/sol.c
@@ -0,0 +1,24 @@
+// switch with examples
+
+#include <stdio.h>
+
+int main(){
+    int  caseNum = 3;
+
+    switch (caseNum){
+        case 1:
+            printf("Choice 1 is selected");
+            break;
+        
+        case 2 :
+            printf("Choice 2 is selected");
+            break;
+        
+        default:
+            printf("No choice found");
+            break;
+        
+
+    }
+    return 0;
+}

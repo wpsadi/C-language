af221bc7fc9c50ca42a019aba1a9e7b9172d6003
Aditya <118204758+wpsadi@users.noreply.github.com>
2023-12-04 11:13:26 +0000

    save

--- Practical/Q016/sol.c
+++ Practical/Q016/sol.c
@@ -1,2 +1,28 @@
 // call by value and call by refernce
 
+#include <stdio.h>
+
+void tnp(float a, float b, float *sum);
+float sums(float a, float b);
+
+int main(){
+    float x = 1.2, y = 1.4;
+
+    float sum;
+
+    tnp(x,y,&sum);
+    printf(" sum with pointer = >%f \n\n",sum);
+    printf(" sum with call by value = >%f \n\n",sums(x,y));
+    return 0;
+}
+
+float sums(float a, float b){
+    return a+b;
+}
+
+void tnp(float a, float b, float *sum){
+
+    *sum = a+b;
+
+
+}

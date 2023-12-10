47f4e273149f395a9e5cebb436e53690d16745b1
wpsadi <adityaislegend@outlook.com>
2023-12-10 21:31:28 +0530

    save

--- Practical/Q022/sol.c
+++ Practical/Q022/sol.c
@@ -1,3 +1,21 @@
 // write fibonacci
 
-#include
+#include <stdio.h>
+int fibo(int); 
+
+int main(){
+    int num = 10;
+    int y = fibo(num)-fibo(num-2);
+    printf("%d",y);
+    return 0;
+}
+
+int fibo(int num){
+    if (num == 0){
+        return 0;
+    }
+    if (num ==1 ){
+        return 1;
+    }
+    return fibo(num-2)+fibo(num-1);
+}
---
+++ Practical/Q023/sol.c
@@ -0,0 +1,9 @@
+//square root
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+    int x = 100;
+    printf("Square root of %d is %f", x, pow(x, 0.5));
+    return 0;
+}
